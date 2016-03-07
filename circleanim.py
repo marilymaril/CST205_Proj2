52e9b0b0bd34f90622a89b959cdf1e3976869ab7
Marilyn <mflorek@csumb.edu>
2016-03-07 13:07:40 -0800

    animation practice

---
+++ circleanim.py
@@ -0,0 +1,16 @@
+import gizeh
+import moviepy.editor as mpy
+
+W,H=128,128 #width,height, in pixels
+duration=2 #duration of the clip, in seconds
+
+def make_frame(t):
+	surface = gizeh.Surface(W,H)
+	radius = W*(1+(t*(duration-t))**2)/6
+	circle = gizeh.circle(radius,xy=(W/2,H/2),fill=(255,0,255))
+	circle.draw(surface)
+	return surface.get_npimage()
+
+clip = mpy.VideoClip(make_frame, duration=duration)
+clip.write_gif("gocircle.gif",fps=15,opt="OptimizePlus",fuzz=10)
+
